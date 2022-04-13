545a20de1baeb80a54bc7e756232fcef0225025c
ipkachung20 <103574475+ipkachung20@users.noreply.github.com>
2022-04-13 19:40:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from transformers import BertForQuestionAnswering, AutoTokenizer
 
-st.write("Hello")
+model = BertForQuestionAnswering.from_pretrained('deepset/bert-base-cased-squad2')
+tokenizer = AutoTokenizer.from_pretrained('deepset/bert-base-cased-squad2')
+
+nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+
+Q = [ "Where do I live?" ]
+context = (" USA live in UK ")
+
+nlp({
+    'question': Q[0],
+    'context': context
+})
