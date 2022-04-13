6e6d8ceab19d7db20b19aa25e7cb93bff2aa88d6
ipkachung20 <103574475+ipkachung20@users.noreply.github.com>
2022-04-13 20:05:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,5 @@ nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
 Q = [ "Where do I live?" ]
 context = (" USA live in UK ")
 
-st.title("Ask Questions about your Text")
-sentence = st.text_area('Please paste your article :', height=30)
-question = st.text_input("Questions from this article?")
-button = st.button("Get me Answers")
-with st.spinner("Discovering Answers.."):
-    if button and sentence:
-        answers = nlp({
-        'question': Q[0],
-        'context': context
-        }).get('answer')
-        
-        st.write(answers)
+answers = nlp({'question': Q[0],'context': context}).get('answer')
+st.write(answers)
