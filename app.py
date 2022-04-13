c993228d5eff49982e86a43a9bf4acdedd4b8144
ipkachung20 <103574475+ipkachung20@users.noreply.github.com>
2022-04-13 20:02:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,9 @@ from transformers import BertForQuestionAnswering, AutoTokenizer, pipeline
 model = BertForQuestionAnswering.from_pretrained('deepset/bert-base-cased-squad2')
 model = AutoTokenizer.from_pretrained('deepset/bert-base-cased-squad2')
 
-@st.cache(allow_output_mutation=True)
-def load_qa_model():
-    model = pipeline('question-answering', model=model, tokenizer=model)
-    return model
-
-qa = load_qa_model()
+nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+Q = [ "Where do I live?" ]
+context = (" USA live in UK ")
 
 st.title("Ask Questions about your Text")
 sentence = st.text_area('Please paste your article :', height=30)
@@ -17,5 +14,9 @@ question = st.text_input("Questions from this article?")
 button = st.button("Get me Answers")
 with st.spinner("Discovering Answers.."):
     if button and sentence:
-        answers = qa(question=question, context=sentence)
-        st.write(answers['answer'])
+        answers = nlp({
+        'question': Q[0],
+        'context': context
+        }).get('answer')
+        
+        st.write(answers)
