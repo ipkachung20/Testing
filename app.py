8be27108b1fe6e5136565e53b8f17ccb52171699
ipkachung20 <103574475+ipkachung20@users.noreply.github.com>
2022-04-13 19:52:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import streamlit as st
 from transformers import BertForQuestionAnswering, AutoTokenizer, pipeline
 
-model = BertForQuestionAnswering.from_pretrained('deepset/bert-base-cased-squad2')
-tokenizer = AutoTokenizer.from_pretrained('deepset/bert-base-cased-squad2')
+model_name = BertForQuestionAnswering.from_pretrained('deepset/bert-base-cased-squad2')
+model_name = AutoTokenizer.from_pretrained('deepset/bert-base-cased-squad2')
 
-nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+@st.cache(allow_output_mutation=True)
+def load_qa_model():
+    model = pipeline('question-answering', model=model_name, tokenizer=model_name)
+    return model
 
-Q = [ "Where do I live?" ]
-context = (" USA live in UK ")
+qa = load_qa_model()
 
-nlp({
-    'question': Q[0],
-    'context': context
-})
-
-st.write(nlp(answer))
+st.title("Ask Questions about your Text")
+sentence = st.text_area('Please paste your article :', height=30)
+question = st.text_input("Questions from this article?")
+button = st.button("Get me Answers")
+with st.spinner("Discovering Answers.."):
+    if button and sentence:
+        answers = qa(question=question, context=sentence)
+        st.write(answers['answer'])
