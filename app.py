adfbd4e9e942d2aab3a5b47bc0320274a35199f0
ipkachung20 <103574475+ipkachung20@users.noreply.github.com>
2022-04-13 20:22:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ model = BertForQuestionAnswering.from_pretrained('deepset/bert-base-cased-squad2
 tokenizer = AutoTokenizer.from_pretrained('deepset/bert-base-cased-squad2')
 
 nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
-Q = [ "Where do I live?" ]
-context = (" USA live in UK ")
 
-answers = nlp({'question': Q[0],'context': context}).get('answer')
-st.write(answers)
+st.title("Ask Questions about your Text")
+context = st.text_area('Please paste your article :', height=30)
+Q = st.text_input("Questions from this article?")
+button = st.button("Get me Answers")
+
+with st.spinner("Discovering Answers.."):
+    if button and sentence:
+        answers = nlp({'question': Q[0],'context': context}).get('answer')
+        st.write(answers['answer'])
